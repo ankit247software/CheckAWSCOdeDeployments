7f37bfa7bad742d8bade3df31db84aec87a58d08
ankit247software <50479169+ankit247software@users.noreply.github.com>
2019-10-15 17:14:26 +0530

    Updated index.js now

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: JSON.stringify('Hello from CheckAWSCOdeDeployments!'),
     };
-    return response;
+    return response; 
 };
 
